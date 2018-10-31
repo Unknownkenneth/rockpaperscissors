8939003ad0fad03edad95f4b94bcfd42ca592ae1
Kenneth Louis <lowkam4@gmail.com>
2018-10-31 21:45:48 +0000

    added if statements

--- js/script.js
+++ js/script.js
@@ -8,4 +8,48 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+var RPS= [rock,paper,scissor]
+var cpu = RPS.random
+var rl
+var pl
+var sl
+var user = input
+var CPU= "#computerChoice"
 
+
+$("#userChoice").click(function(){});
+    
+    
+$("shoot").click(function(){
+    
+     if(user && CPU === rock || paper || scissor){ 
+        console.log("Draw")        
+      } else if ( user === rock && CPU === paper ){
+        console.log(Loser)
+    }
+    
+     else if ( user === paper ||scissor && CPU === scissor ){
+        console.log(Loser)
+    }
+     else if ( user === scissor && CPU === rock ){
+        console.log(Loser)
+    }
+     else if ( user === rock  && CPU === scissor ){
+        console.log(Loser)
+    }
+     else if ( user === rock || paper ||scissor && CPU === paper ){
+        console.log(Loser)
+    }
+     else if ( user === rock || paper ||scissor && CPU === paper ){
+        console.log(Loser)
+    }
+       else if(){
+           
+           
+       
+    }
+    
+    $("result").text($("winner").val());
+    
+ 
+});
